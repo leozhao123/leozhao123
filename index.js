194ba1f72654bd0db8d2e24543e8b3393275e7a2
leozhao123 <88172030+leozhao123@users.noreply.github.com>
2021-07-30 13:42:07 +1000

    added function added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// change branch need use git push -u origin <branch> //
+
+
+
 console.log("Hello World");
 const addingnumbers = (x,y) => {return "new number is "  + x+y};
 console.log(addingnumbers(5,10));
